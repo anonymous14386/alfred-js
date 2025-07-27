29c929c1d5bf27080344b2c777c6eb9c9ad16dd7
Nick <ndiramio@gmail.com>
2025-07-26 23:20:37 -0400

    duplicate require

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const config = require('./config.json');
 
 require('dotenv').config(); 
-const fs = require('node:fs');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
